85e37bd75267bc11034cd98572673923442bc4e1
Rahma, A. Z. R <118007550+alyazahrarr@users.noreply.github.com>
2023-11-18 01:01:39 +0700

    tp modul 2

--- tugas-pendahuluan/TP Modul 2_13022085_Alya Zahra Raisa Rahma/Soal 1.cpp
+++ tugas-pendahuluan/TP Modul 2_13022085_Alya Zahra Raisa Rahma/Soal 1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-#include <vector>
+#include <array>
 
 // TP Modul 2 Programming
 // Nama : Alya Zahra Raisa Rahma
@@ -11,5 +11,25 @@ using namespace std;
 
 int main()
 {
+    cout << "Input array 1:" << endl;
+    array<int, 10> arr {};
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Input array 2:" << endl;
+    array<int, 10> arr2 {};
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> arr2[i];
+    }
+
+    cout << "Hasil penjumlahan: ";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i] + arr2[i] << " ";
+    }
+    
     return 0;
 }
