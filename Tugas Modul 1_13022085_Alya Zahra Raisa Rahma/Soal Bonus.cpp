c2c7aa094d3acdf8b7d4950b4414173a7b3a1e1a
Rahma, A. Z. R <118007550+alyazahrarr@users.noreply.github.com>
2023-11-17 16:27:32 +0700

    update tugas

--- Tugas Modul 1_13022085_Alya Zahra Raisa Rahma/Soal Bonus.cpp
+++ Tugas Modul 1_13022085_Alya Zahra Raisa Rahma/Soal Bonus.cpp
@@ -9,5 +9,43 @@ using namespace std;
 
 int main()
 {
+    int N;
+    cout << "N = ";
+    cin >> N;
+
+    int num = 0;
+
+    if (!(N % 2 == 0)){ // kalau ganjil
+        N = N + 1;}
+    else {N = N + 2;}
+
+    for (int j = 1; j <= N-1; j++) //baris
+    {
+        if (j < N/2){
+            for (int i = 0; i < j - 1; i++){
+                cout << " ";}
+            for (int i = 0; i < N/2 - j + 1; i++){
+                cout << num;
+                num = num + 1;
+                if (num == 10){num = 0;}}
+            cout << endl;}
+        if (j == N/2){
+            for (int i = 0; i < N/2 - 1; i++){
+                cout << " ";}
+            cout << num;
+            num = num + 1;
+            if (num == 10){num = 0;}               
+            cout << endl;}
+        if (j > N/2){
+            for (int i = 0; i < N/2 - 1 ; i++){
+                cout << " ";}
+            for (int i = 0; i < j + 1 - N/2; i++){
+                cout << num;
+                num = num + 1;
+                if (num == 10){num = 0;}}
+            cout << endl;}
+    }
+    
+
     return 0;
 }
---
+++ tugas-pendahuluan/TP Modul 2_13022085_Alya Zahra Raisa Rahma/Soal 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+#include <vector>
+
+// TP Modul 2 Programming
+// Nama : Alya Zahra Raisa Rahma
+// NIM  : 13022085
+
+// Soal 1 : Array
+
+int main()
+{
+    return 0;
+}
